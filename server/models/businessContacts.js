c16efcae777b480602c62fad485f888cad9f4867
aaujla55 <amandeep99555@gmail.com>
2017-03-10 16:13:22 -0500

    model updated

---
+++ server/models/businessContacts.js
@@ -0,0 +1,13 @@
+let mongoose = require('mongoose');
+
+// create a model class
+let businessContactSchema = mongoose.Schema({
+    name: String,
+    email: String,
+    contact: Number
+},
+{
+  collection: "businesscontacts"
+});
+
+module.exports = mongoose.model('businesscontacts', businessContactSchema);
