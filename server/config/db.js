c240a30e13d5610082e609ceb3ef17a052742f46
aaujla55 <amandeep99555@gmail.com>
2017-03-10 21:25:01 -0500

    mlab configured

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,6 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/mybusinesscontacts"
+  //"URI": "mongodb://localhost/mybusinesscontacts"
   //Remote MongoDB deployment -> let URI = "mongodb://thomas:123456@ds054999.mlab.com:54999/videogames";
+  "URI" : "mongodb://aaujla55:Amandeep9$@ds021172.mlab.com:21172/mybusinesscontacts"
 };
